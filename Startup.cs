5b0cf88496ee94772744a997365b0376eaa6100b
AndersPamp <anders_pamp@hotmail.com>
2019-08-30 10:09:25 +0200

    change entities

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,13 @@
 using HbgKontoret.Data.Helpers;
 using HbgKontoret.Data.Service.Interfaces;
 using HbgKontoret.Data.Services;
+using HbgKontoret.Data.Data;
+using HbgKontoret.Data.Data.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -24,8 +27,14 @@ namespace HbgKontoret
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
       services.AddCors();
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+      services.AddScoped<LoginRepository>();
+      services.AddScoped<ILoginService, LoginService>();
 
       var appSettingsSection = Configuration.GetSection("AppSettings");
       services.Configure<AppSettings>(appSettingsSection);
@@ -49,7 +58,7 @@ namespace HbgKontoret
         };
       });
 
-      services.AddScoped<IUserService, UserService>();
+      services.AddScoped<ILoginService, LoginService>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
